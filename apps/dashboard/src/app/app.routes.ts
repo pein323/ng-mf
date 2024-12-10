1b6b250771faf7f0f768064d4f45dc03254944bd
Krzysztof Bistram <bistram.k@gmail.com>
2024-12-10 11:47:29 +0100

    added second remote app

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'profile',
+    loadChildren: () => import('profile/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'login',
     loadChildren: () => import('login/Routes').then((m) => m!.remoteRoutes),
   },
