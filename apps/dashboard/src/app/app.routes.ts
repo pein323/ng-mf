dcf6b469dd47a2be2a2cf89cf0711440c70bacc9
Krzysztof Bistram <bistram.k@gmail.com>
2024-12-10 18:04:45 +0100

    run the app

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,17 +1,13 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { Route } from '@angular/router';
+import {Route} from '@angular/router';
+import {AppComponent} from './app.component';
 
 export const appRoutes: Route[] = [
   {
-    path: 'profile',
-    loadChildren: () => import('profile/Routes').then((m) => m!.remoteRoutes),
-  },
-  {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () => import('login/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
-    component: NxWelcomeComponent,
+    component: AppComponent,
   },
 ];
--- apps/login/src/app/remote-entry/entry.component.ts
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {UserService} from '@ng-mf/data-access-user';
 
 @Component({
-  imports: [CommonModule, NxWelcomeComponent],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   selector: 'ng-mf-login-entry',
-  template: `<ng-mf-nx-welcome></ng-mf-nx-welcome>`,
+  template: `
+    <div class="login-app">
+      <form class="login-form" (ngSubmit)="login()">
+        <label>
+          Username:
+          <input type="text" name="username" [(ngModel)]="username"/>
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" [(ngModel)]="password"/>
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <div *ngIf="isLoggedIn$ | async">User is logged in!</div>
+    </div>
+  `,
+  styles: [
+    `
+      .login-app {
+        width: 30vw;
+        border: 2px dashed black;
+        padding: 8px;
+        margin: 0 auto;
+      }
+
+      .login-form {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin: 0 auto;
+        padding: 8px;
+      }
+
+      label {
+        display: block;
+      }
+    `,
+  ],
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  username = '';
+  password = '';
+  private userService = inject(UserService);
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  login() {
+    this.userService.checkCredentials(this.username, this.password);
+  }
+}
