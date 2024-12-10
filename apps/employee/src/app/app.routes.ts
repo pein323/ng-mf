e921404f778eafd6721579677942e743723baaa5
Krzysztof Bistram <bistram.k@gmail.com>
2024-12-10 18:10:20 +0100

    added dynamic remote

--- apps/employee/src/app/app.routes.ts
+++ apps/employee/src/app/app.routes.ts
@@ -4,6 +4,13 @@ import { loadRemote } from '@module-federation/enhanced/runtime';
 
 export const appRoutes: Route[] = [
   {
+    path: 'todo',
+    loadChildren: () =>
+      loadRemote<typeof import('todo/Routes')>('todo/Routes').then(
+        (m) => m!.remoteRoutes
+      ),
+  },
+  {
     path: 'login',
     loadChildren: () =>
       loadRemote<typeof import('login/Routes')>('login/Routes').then(
