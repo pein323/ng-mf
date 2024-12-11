4271e0712fe31b9fe1c5ab4710a0660c2f5cec97
Krzysztof Bistram <bistram.k@gmail.com>
2024-12-11 13:14:59 +0100

    excluded shared libs

--- apps/login/module-federation.config.ts
+++ apps/login/module-federation.config.ts
@@ -1,9 +1,25 @@
-import { ModuleFederationConfig } from '@nx/module-federation';
+import {ModuleFederationConfig} from '@nx/module-federation';
 
 const config: ModuleFederationConfig = {
   name: 'login',
   exposes: {
     './Routes': 'apps/login/src/app/remote-entry/entry.routes.ts',
+  },
+  shared: (name, config) => {
+    // We want lodash to be tree shaken, and bundled into each host/remote separately.
+    switch (name) {
+      case 'rxjs':
+      case 'rxjs/operators':
+      //case '@angular/router':
+      case '@angular/platform-browser':
+      case '@angular/forms':
+      //case '@angular/core':
+      case '@angular/core/primitives/signals':
+      case '@angular/core/primitives/event-dispatch':
+      //case '@angular/common':
+      case '@angular/common/http':
+        return false
+    }
   },
 };
 
